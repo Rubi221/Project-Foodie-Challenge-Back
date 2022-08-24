4189773ebba52f1e2789f91ea9c296aca76889e1
Rubi221 <98795896+Rubi221@users.noreply.github.com>
2022-08-23 19:37:07 -0500

    Proyecto Back

---
+++ FoodieChallengeBack/src/main/java/com/example/foodiechallengeback/FoodieChallengeBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodiechallengeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieChallengeBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodieChallengeBackApplication.class, args);
+    }
+
+}
---
+++ FoodieChallengeBack/src/test/java/com/example/foodiechallengeback/FoodieChallengeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodiechallengeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodieChallengeBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
